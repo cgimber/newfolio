12a79f2249c9a828947dc49ccaedfceba6db1411
Christian Gimber <cgimber@ucla.edu>
2017-10-10 23:31:42 -0700

    testing new layout

--- js/main.js
+++ js/main.js
@@ -4,12 +4,35 @@
 /* document ready
 ---------------------------------------------------------------------*/
 $(document).ready(function() {
-	$('video').hover(function() {
-		this.play();
-	}, function() {
-		this.pause();
-		this.currentTime = 0;
-	});
+    // $('video').hover(function() {
+    // 	this.play();
+    // }, function() {
+    // 	this.pause();
+    // 	this.currentTime = 0;
+    // });
+    
+    var $carousel = $('.carousel').flickity({
+        // options
+        cellAlign: 'center',
+	    initialIndex: 3,
+        imagesLoaded: true,
+        prevNextButtons: false,
+        pageDots: false,
+        draggable: true,
+        wrapAround: false
+    });
+    var $slider = $('.slider');
+    var flkty = $carousel.data('flickity');
+
+    $carousel.on('select.flickity', function() {
+        // update the range slider
+        $slider.val(flkty.selectedIndex);
+    });
+
+    $slider.on('input', function() {
+        // update the flickity slider
+        flkty.select(this.value, false, false);
+    });
 });
 
 /* functions
